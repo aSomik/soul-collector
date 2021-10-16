cdd0b914eca5d9e1a94d7bb355ec02d4a5b0966e
aSomik <asdburn@mail.ru>
2021-10-16 21:35:22 +0300

    sprites, mostly

--- src/main/java/soulcollector/items/base/Bebrock.java
+++ src/main/java/soulcollector/items/base/Bebrock.java
@@ -1,9 +1,25 @@
 package soulcollector.items.base;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class Bebrock extends Item {
     public Bebrock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(new TranslatableText("item.soulcoll.bebrock_ingot.tooltip").formatted(Formatting.DARK_GRAY));
+        tooltip.add(new TranslatableText("item.soulcoll.bebrock_ingot.tooltip1").formatted(Formatting.DARK_GRAY));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
