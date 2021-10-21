88052f9043295d7a47a3ca06b91d24a2369dd284
aSomik <asdburn@mail.ru>
2021-10-21 22:23:31 +0300

    among us

--- src/main/java/soulcollector/items/SoulCollectorItems.java
+++ src/main/java/soulcollector/items/SoulCollectorItems.java
@@ -15,10 +15,10 @@ public class SoulCollectorItems {
     public static final Map<Identifier, Item> ITEMS = new LinkedHashMap<>();
 
     public static final Item STEEL_INGOT = add("steel_ingot", new BaseItem(settings1()));
+    public static final Item ANCIENT_SOUL = add("ancient_soul", new AncientSoul(settings1()));
     public static final Item SOUL_INGOT = add("soul_ingot", new SoulIngot(settings1()));
     public static final Item BEBROCK = add("bebrock_ingot", new Bebrock(settings1()));
     public static final Item PIG_CROWN = add("pig_crown", new TechnoCrown(settings1()));
-    public static final Item ANCIENT_SOUL = add("ancient_soul", new AncientSoul(settings1()));
     public static final Item INACTIVE_SINGULARITY = add("inactive_singularity", new Singularity(settings1()));
     public static final Item FRIENDLY_SINGULARITY = add("fren_singularity", new Singularity(settings1()));
     public static final Item PEACEFUL_SINGULARITY = add("peaceful_singularity", new Singularity(settings1()));
